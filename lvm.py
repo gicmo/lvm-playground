37c165b9431413b156e7b6985fd972e5d88ca665
Christian Kellner <christian@kellner.me>
2021-07-26 17:16:20 +0200

    lvm: use crc implemenation from python
    There is some inversion between python and lvm's way of crc32
    calculation.

--- lvm.py
+++ lvm.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import abc
+import binascii
 import io
 import json
 import os
@@ -20,17 +21,9 @@ MDA_HEADER_SIZE = 512
 
 
 def _calc_crc(buf, crc=INITIAL_CRC):
-    crctab = [0x00000000, 0x1db71064, 0x3b6e20c8, 0x26d930ac,
-              0x76dc4190, 0x6b6b51f4, 0x4db26158, 0x5005713c,
-              0xedb88320, 0xf00f9344, 0xd6d6a3e8, 0xcb61b38c,
-              0x9b64c2b0, 0x86d3d2d4, 0xa00ae278, 0xbdbdf21c]
-
-    for b in buf:
-        crc ^= b  # crc ^= *buf++;
-        crc = (crc >> 4) ^ crctab[crc & 0xf]
-        crc = (crc >> 4) ^ crctab[crc & 0xf]
-
-    return crc
+    crc = crc ^ 0xFFFFFFFF
+    crc = binascii.crc32(buf, crc)
+    return crc ^ 0xFFFFFFFF
 
 
 class CStruct:
