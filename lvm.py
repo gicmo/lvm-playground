a44b996f6628d917a7f046a58c0887f6ad58d1e4
Christian Kellner <christian@kellner.me>
2021-07-26 10:10:16 +0200

    lvm: refactor to have cleaner code

--- lvm.py
+++ lvm.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 
-import binascii
 import io
+import os
+import re
 import struct
+import sys
 
-from typing import Dict, BinaryIO
+from typing import BinaryIO, Dict, Union
 
 # https://github.com/lvmteam/lvm2/blob/8801a86a3e0c87d92b250a6477f86ef9efdb2ba0/lib/format_text/format-text.c
+
+PathLike = Union[str, bytes, os.PathLike]
 
 INITIAL_CRC = 0xf597a6cf
 MDA_HEADER_SIZE = 512
@@ -47,88 +51,102 @@ class CStruct:
     def size(self):
         return self.struct.size
 
-    def read(self, fp: BinaryIO):
-        pos = fp.tell()
-        data = fp.read(self.size)
+    def unpack(self, data):
         up = self.struct.unpack_from(data)
         res = {
             field.name: up[idx]
             for idx, field in enumerate(self.fields)
         }
+        return res
+
+    def read(self, fp):
+        pos = fp.tell()
+        data = fp.read(self.size)
+
+        if len(data) < self.size:
+            return None
+
+        res = self.unpack(data)
         res["_position"] = pos
         return res
 
-    def write(self, fp: BinaryIO, data: Dict):
+    def pack(self, data):
         values = [
             data[field.name] for field in self.fields
         ]
         data = self.struct.pack(*values)
-        fp.write(data)
+        return data
 
-    def iter(self, fp):
-        while True:
-            yield self.read(fp)
+    def write(self, fp, data: Dict, *, offset=None):
+        packed = self.pack(data)
 
+        save = None
+        if offset:
+            save = fp.tell()
+            fp.seek(offset)
 
-# /* On disk - 32 bytes */
-# struct label_header {
-#     int8_t id[8];		/* LABELONE */
-#     uint64_t sector_xl;	/* Sector number of this label */
-#     uint32_t crc_xl;	/* From next field to end of sector */
-#     uint32_t offset_xl;	/* Offset from start of struct to contents */
-#     int8_t type[8];		/* LVM2 001 */
-# } __attribute__ ((packed));
-HeaderStruct = CStruct({
-    "id": "8s",
-    "sector": "Q",
-    "crc": "L",
-    "offset": "L",
-    "type": "8s"
-})
+        fp.write(packed)
 
-DiskLocNStruct = CStruct({
-    "offset": "Q",
-    "size": "Q"
-})
+        if save:
+            fp.seek(save)
 
-RawLocnStruct = CStruct({
-    "offset": "Q",
-    "size": "Q",
-    "checksum": "L",
-    "filler": "L"
-})
+    def __getitem__(self, name):
+        for f in self.fields:
+            if f.name == f:
+                return f
+        raise KeyError(f"Unknown field '{name}'")
 
-MDAHeaderStruct = CStruct({
-    "checksum": "L",
-    "magic": "16s",
-    "version": "L",
-    "start": "Q",
-    "size": "Q"
-})
+    def __contains__(self, name):
+        return any(field.name == name for field in self.fields)
 
 
 class LabelHeader:
 
+    # /* On disk - 32 bytes */
+    # struct label_header {
+    #     int8_t id[8];		     /* LABELONE */
+    #     uint64_t sector_xl;	 /* Sector number of this label */
+    #     uint32_t crc_xl;	     /* From next field to end of sector */
+    #     uint32_t offset_xl;	 /* Offset from start of struct to contents */
+    #     int8_t type[8];		 /* LVM2 001 */
+    # } __attribute__ ((packed));
+
+    struct = CStruct({
+        "id": "8s",
+        "sector": "Q",
+        "crc": "L",
+        "offset": "L",
+        "type": "8s"
+    })
+
     LABELID = b"LABELONE"
-    binary = struct.Struct("<8sQLL8s")
 
-    def __init__(self, data, position):
-        self.position = position
+    # scan sector 0 to 3 inclusive
+    LABEL_SCAN_SECTORS = 4
 
-        unpacked = self.binary.unpack_from(data)
-        self.sector = unpacked[1]
-        self.crc = unpacked[2]
-        self.offset = unpacked[3]
-        self.type = unpacked[4]
+    def __init__(self, data):
+        up = self.struct.unpack(data)
 
-    @classmethod
-    def search(cls, fp):
+        self.sector_size = 512
+
+        self.sector = up["sector"]
+        self.crc = up["crc"]
+        self.offset = up["offset"]
+        self.type = up["type"]
+
+    @ classmethod
+    def search(cls, fp, *, sector_size=512):
         fp.seek(0, io.SEEK_SET)
-        for i in range(3):
-            data = fp.read(512)
-            if data[0:8] == cls.LABELID:
-                return LabelHeader(data, i*512)
+        for i in range(cls.LABEL_SCAN_SECTORS):
+            data = fp.read(sector_size)
+            if data[0:len(cls.LABELID)] == cls.LABELID:
+                return LabelHeader(data)
         return None
+
+    def read_pv_header(self, fp):
+        offset = self.sector * self.sector_size + self.offset
+        fp.seek(offset)
+        return PVHeader.read(fp)
 
     def __str__(self):
         return f"LabelHeader: {self.sector}, {self.crc}, {self.offset}, {self.type}"
@@ -141,7 +159,10 @@ class DiskLocN:
         uint64_t size;		/* Bytes */
     } __attribute__ ((packed));
     """
-    binary = struct.Struct("<QQ")
+    struct = CStruct({
+        "offset": "Q",
+        "size": "Q"
+    })
 
     def __init__(self, offset, size):
         self.offset = offset
@@ -150,19 +171,20 @@ class DiskLocN:
     def __str__(self):
         return f"DiskLocN: {self.offset}, {self.size}"
 
+    def read_data(self, fp: BinaryIO):
+        fp.seek(self.offset)
+        data = fp.read(self.size)
+        return io.BytesIO(data)
+
     @classmethod
-    def read(cls, fp):
+    def read_array(cls, fp):
         while True:
-            data = fp.read(cls.binary.size)
+            data = cls.struct.read(fp)
 
-            if len(data) < cls.binary.size:
+            if not data or data["offset"] == 0:
                 break
 
-            offset, size = cls.binary.unpack_from(data)
-            if offset == 0:
-                break
-
-            yield DiskLocN(offset, size)
+            yield DiskLocN(data["offset"], data["size"])
 
 
 class PVHeader:
@@ -178,72 +200,344 @@ class PVHeader:
         struct disk_locn disk_areas_xl[];	/* Two lists */
     } __attribute__ ((packed));
     """
+    struct = CStruct({
+        "uuid": "32s",
+        "disk_size": "Q"
+    })
 
-    ID_LEN = 32
-    binary = struct.Struct("<32sQ")
+    def __init__(self, data, data_areas, meta_areas):
+        self.uuid = data["uuid"]
+        self.disk_size = data["disk_size"]
 
-    def __init__(self, fp, lbl: LabelHeader):
-        offset = lbl.position + lbl.offset
-        fp.seek(offset, io.SEEK_SET)
-        data = fp.read(self.binary.size)
-        up = self.binary.unpack_from(data)
-        self.uuid = up[0]
+        self.data_areas = data_areas
+        self.meta_areas = meta_areas
 
-        self.data_areas = list(DiskLocN.read(fp))
-        self.metadata_areas = list(DiskLocN.read(fp))
+    @classmethod
+    def read(cls, fp):
+        data = cls.struct.read(fp)
+
+        data_areas = list(DiskLocN.read_array(fp))
+        meta_areas = list(DiskLocN.read_array(fp))
+
+        return cls(data, data_areas, meta_areas)
 
     def __str__(self):
-        msg = f"PVHeader: {self.uuid} "
-        msg += "\nData: \n\t" + "\n\t".join(map(str, self.data_areas))
-        msg += "\nMeta: \n\t" + "\n\t".join(map(str, self.metadata_areas))
+        msg = f"PVHeader(uuid: {self.uuid}, {self.disk_size})"
+        if self.data_areas:
+            msg += "\nData: \n\t" + "\n\t".join(map(str, self.data_areas))
+        if self.meta_areas:
+            msg += "\nMeta: \n\t" + "\n\t".join(map(str, self.meta_areas))
         return msg
 
-    def metadata_headers(self, fp):
-        for ma in self.metadata_areas:
-            fp.seek(ma.offset)
-            md = MDAHeaderStruct.read(fp)
-            print("Metadata", md["version"], md["start"], md["size"])
 
-            raw = []
-            for loc in RawLocnStruct.iter(fp):
-                if loc["offset"] == 0:
-                    break
-                raw.append(loc)
+class RawLocN:
+    struct = CStruct({
+        "offset": "Q",
+        "size": "Q",
+        "checksum": "L",
+        "flags": "L",
+    })
 
-            r = raw[0]
-            offset = md["start"] + r["offset"]
-            fp.seek(offset)
-            data = fp.read(r["size"])
-            print(data)
-            print(r["checksum"])
-            print(_calc_crc(data))
-            data = b"ck" + data[2:]
-            # print(data)
-            fp.seek(offset)
-            fp.write(data)
+    IGNORED = 0x00000001
 
-            r["checksum"] = _calc_crc(data)
-            print(r["checksum"])
-            fp.seek(r["_position"])
-            RawLocnStruct.write(fp, r)
+    def __init__(self, data) -> None:
+        self.data = data
 
-            cdata = struct.Struct("<L")
-            fp.seek(ma.offset + cdata.size)
-            data = fp.read(MDA_HEADER_SIZE - cdata.size)
-            checksum = cdata.pack(_calc_crc(data))
-            fp.seek(ma.offset)
-            fp.write(checksum)
-            fp.write(data)
+    def __getitem__(self, name):
+        assert name in self.struct
+        return self.data[name]
+
+    def __setitem__(self, name, value):
+        assert name in self.struct
+        self.data[name] = value
+
+    def write(self, fp):
+        raw = self.struct.pack(self.data)
+        n = fp.write(raw)
+        return n
+
+    @classmethod
+    def read_array(cls, fp: BinaryIO):
+        while True:
+            loc = cls.struct.read(fp)
+
+            if not loc or loc["offset"] == 0:
+                break
+
+            yield cls(loc)
+
+    def __str__(self):
+        s, o = self["size"], self["offset"]
+        return f"RawLocN ({s} bytes @ {o}) ({id(self.data)})"
+
+
+class MDAHeader:
+    struct = CStruct({
+        "checksum": "L",
+        "magic": "16s",
+        "version": "L",
+        "start": "Q",
+        "size":  "Q"
+    })
+
+    LOC_COMMITTED = 0
+    LOC_PRECOMMITTED = 1
+
+    HEADER_SIZE = MDA_HEADER_SIZE
+
+    def __init__(self, data, raw_locns):
+        self.data = data
+        self.raw_locns = raw_locns
+
+    @property
+    def checksum(self):
+        return self.data["checksum"]
+
+    @property
+    def magic(self):
+        return self.data["magic"]
+
+    @property
+    def version(self):
+        return self.data["version"]
+
+    @property
+    def start(self):
+        return self.data["start"]
+
+    @property
+    def size(self):
+        return self.data["size"]
+
+    @classmethod
+    def read(cls, fp):
+        data = cls.struct.read(fp)
+        raw_locns = list(RawLocN.read_array(fp))
+        return cls(data, raw_locns)
+
+    def read_metadata(self, fp):
+        loc = self.raw_locns[self.LOC_COMMITTED]
+        offset = self.start + loc["offset"]
+        fp.seek(offset)
+        data = fp.read(loc["size"])
+        print(data)
+        data = data.decode("utf-8")
+        md = Metadata.parse(data)
+        return md
+
+    def write_metadata(self, fp, data):
+        strdata = Metadata.encode(data) + "\0"
+        raw = strdata.encode("utf-8")
+
+        loc = self.raw_locns[self.LOC_COMMITTED]
+        offset = self.start + loc["offset"]
+        fp.seek(offset)
+
+        n = fp.write(raw)
+        loc["size"] = n
+        loc["checksum"] = _calc_crc(raw)
+        self.write(fp)
+
+    def write(self, fp):
+        data = self.struct.pack(self.data)
+
+        fr = io.BytesIO()
+        fr.write(data)
+
+        for loc in self.raw_locns:
+            loc.write(fr)
+
+        l = fr.tell()
+        fr.write(b"\0" * (self.HEADER_SIZE - l))
+
+        raw = fr.getvalue()
+
+        cs = struct.Struct("<L")
+        checksum = _calc_crc(raw[cs.size:])
+        self.data["checksum"] = checksum
+        data = self.struct.pack(self.data)
+        fr.seek(0)
+        fr.write(data)
+
+        fp.seek(self.start)
+        n = fp.write(fr.getvalue())
+        print(n)
+        return n
+
+    def __str__(self):
+        msg = f"MDA: crc: {self.checksum}, {self.size} bytes @ {self.start}"
+        if self.raw_locns:
+            msg += "\n\t" + "\n\t".join(map(str, self.raw_locns))
+        return msg
+
+
+class Metadata:
+    def __init__(self, header: MDAHeader, data: Dict) -> None:
+        self.header = header
+        self.data = data
+
+    @staticmethod
+    def parse(raw):
+        substitutions = {
+            r"#.*\n": "",
+            r"\[": "[ ",
+            r"\]": " ]",
+            r'"': ' " ',
+            r"[=,]": "",
+            r"\s+":  " ",
+            r"\0$": "",
+        }
+
+        data = raw
+        for pattern, repl in substitutions.items():
+            data = re.sub(pattern, repl, data)
+
+        data = data.split()
+        print(data)
+
+        DICT_START = '{'
+        DICT_END = '}'
+        ARRAY_START = '['
+        ARRAY_END = ']'
+        STRING_START = '"'
+        STRING_END = '"'
+
+        def parse_str(val):
+            result = ""
+
+            while val != STRING_END:
+                result = f"{result} {val}"
+                val = data.pop(0)
+
+            return result.strip()
+
+        def parse_val(val):
+            if val == STRING_START:
+                return parse_str(data.pop(0))
+            return int(val)
+
+        def parse_array(val):
+            result = []
+
+            while val != ARRAY_END:
+                val = parse_val(val)
+                result.append(val)
+                val = data.pop(0)
+
+            return result
+
+        def parse_dict(val):
+            result = {}
+            while val != DICT_END:
+                result[val] = parse_obj()
+                if not data:
+                    return result
+                val = data.pop(0)
+            return result
+
+        def parse_obj():
+
+            val = data.pop(0)
+
+            if val == DICT_START:
+                return parse_dict(data.pop(0))
+            elif val == ARRAY_START:
+                return parse_array(data.pop(0))
+            else:
+                val = parse_val(val)
+
+            return val
+
+        name = data.pop(0)
+        obj = parse_dict(name)
+
+        return obj
+
+    @staticmethod
+    def encode(data):
+
+        b = ""
+
+        def encode_dict(d):
+            s = ""
+            for k, v in d.items():
+                s += k
+                if not isinstance(v, dict):
+                    s += " = "
+                else:
+                    s += " "
+                s += encode_val(v) + "\n"
+            return s
+
+        def encode_val(v):
+            if isinstance(v, int):
+                s = str(v)
+            elif isinstance(v, str):
+                s = f'"{v}"'
+            elif isinstance(v, list):
+                s = "[" + ", ".join(encode_val(x) for x in v) + "]"
+            elif isinstance(v, dict):
+                s = '{\n'
+                s += encode_dict(v)
+                s += '}\n'
+            return s
+
+        return encode_dict(data)
+
+
+class Disk:
+    def __init__(self, fp, path: PathLike, lbl: LabelHeader) -> None:
+        self.fp = fp
+        self.path = path
+
+        self.lbl_hdr = lbl
+        self.pv_hdr = lbl.read_pv_header(fp)
+
+    @ classmethod
+    def open(cls, path: PathLike) -> None:
+        fp = open(path, "rb+")
+        hdr = LabelHeader.search(fp)
+
+        if not hdr:
+            raise RuntimeError("Could not find label header")
+
+        return cls(fp, path, hdr)
+
+    def read_metadata(self):
+        pv = self.pv_hdr
+
+        for ma in pv.meta_areas:
+            data = ma.read_data(self.fp)
+            hdr = MDAHeader.read(data)
+            md = hdr.read_metadata(self.fp)
+            print(hdr)
+            print(md)
+
+            hdr.write_metadata(self.fp, md)
+            md = hdr.read_metadata(self.fp)
+            print(hdr)
+
+    def dump(self):
+        print(self.path)
+        print(self.lbl_hdr)
+        print(self.pv_hdr)
+
+    def __enter__(self):
+        assert self.fp, "Disk not open"
+        return self
+
+    def __exit__(self, *exc_details):
+        if self.fp:
+            self.fp.flush()
+            self.fp.close()
+            self.fp = None
 
 
 def main():
 
-    with open("image.img", "rb+") as img:
-        hdr = LabelHeader.search(img)
-        print(hdr)
-        pv = PVHeader(img, hdr)
-        print(pv)
-        pv.metadata_headers(img)
+    with Disk.open(sys.argv[1]) as disk:
+        disk.dump()
+        disk.read_metadata()
 
 
 if __name__ == "__main__":
